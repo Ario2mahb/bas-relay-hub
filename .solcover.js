20a1d10cfe18acc068899720ec46199bb3edf41a
Ario2mahb <138014505+Ario2mahb@users.noreply.github.com>
2024-06-01 13:19:30 +0330

    Update .solcover.js

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   providerOptions: {
     _chainId: 1337,
-    _chainIdRpc: 1337,
+    _chainIdRpc: 8765,
   },
   client: require("ganache-cli"), // Will load the outermost ganache-cli in node_modules
   skipFiles: [
